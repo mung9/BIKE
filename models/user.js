73026f4fd053539cd3e0d87a1ce4ca67860d74f8
mung9 <cutiemung2@naver.com>
2019-06-06 19:27:11 +0900

    email regex 수정

--- models/user.js
+++ models/user.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
     required: [true, "username is required."],
     validate: {
       validator: v => {
-        const emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+        const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return emailRegex.test(v);
       },
       message: props => `${props.value} is invalid email form.`
